e476bea4d4e5b0dd19c5770f6ad47f4de962ef0d
Simoes, M. R <49722441+SimoesMR@users.noreply.github.com>
2023-08-15 20:59:09 -0300

    ajuste layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-"use client";
+import React, { useState } from "react";
 import Header from "@/components/header";
-import "../styles/globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
-export const unstable_serialization = true; // Aqui
 
+// Essas declarações ficam fora do componente
+export const unstable_serialization = true;
 export const metadata: Metadata = {
   title: "Rodrigo M Simoes",
   description: "Aprsentação de Rodrigo e seus trabalhos",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
+
   return (
     <html lang="pt-Br">
       <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
