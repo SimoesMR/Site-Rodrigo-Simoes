129f309532956b67a1dd35bf67a2f80778aaa11d
Simoes, M. R <49722441+SimoesMR@users.noreply.github.com>
2023-08-15 18:00:16 -0300

    add output
    add output: "export",

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export", // Habilita a exportação estática
   basePath: process.env.NEXT_PUBLIC_PDF_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_PDF_PATH,
   env: {
